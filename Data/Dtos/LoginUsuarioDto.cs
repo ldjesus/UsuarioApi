a06f22df9887e4c5b80bd2950dff356ab40d8254
Leandro <ufmt.leandro@gmail.com>
2023-09-27 06:23:41 -0400

    primeiro commite

---
+++ Data/Dtos/LoginUsuarioDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Dtos
+{
+    public class LoginUsuarioDto
+    {
+        [Required]
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
